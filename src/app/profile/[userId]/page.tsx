9e34c2a2f7ef8fd03e9414940d23110ad6e69a6d
Javier Gil Calvo <jgil@isdefe.es>
2024-09-04 15:14:28 +0200

    Task Manager Initial Commit

---
+++ src/app/profile/[userId]/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <>
+      <div>Datos del usuario</div>
+    </>
+  );
+}
---
+++ src/app/task-list/page.tsx
@@ -0,0 +1,5 @@
+import { TaskManager } from "@/components/ui/TaskManager";
+
+export default function Tasks() {
+  return <TaskManager />;
+}
